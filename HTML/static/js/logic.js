357a1d864d5fd4f85110f60de53d65321dd490fd
Adrian <azmodea@gmail.com>
2020-11-28 13:26:21 -0600

    Backing up works cited progress

--- HTML/static/js/logic.js
+++ HTML/static/js/logic.js
@@ -18,3 +18,25 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+d3.csv("oneauthor_references.csv").then(function(citedData) {
+  console.log(citedData)
+
+  citedData.forEach(function(data) {
+    // data.author1first = data.author1first;
+    data.author1last = data.author1last;
+    data.pubyr = +data.pubyr;
+    data.reftitle = data.reftitle.toString();
+  });
+
+  console.log(citedData.reftitle)
+  var cited = d3.select("#works_cited")
+  var works = d3.select("ul");
+
+  var table = cited.selectAll("ul")
+    .data(citedData)
+    .enter()
+    .append("ol")
+    .text(d => `${d.author1last}` + " " + `${d.pubyr},` + " " + `${d.reftitle.toString()}, vol. ${d.pubvol}, no. ${d.pubno},` + " " +`pp. ${d.firstpage}-${d.lastpage}`)
+
+})

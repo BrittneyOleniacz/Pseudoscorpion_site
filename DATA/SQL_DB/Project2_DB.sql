79b382d2b5a45f42556fb3f50c19bd4001ff38ad
Brittney Oleniacz <brittneyoleniacz@gmail.com>
2020-11-28 13:44:11 -0600

    ...

--- DATA/SQL_DB/Project2_DB.sql
+++ DATA/SQL_DB/Project2_DB.sql
@@ -1,63 +1,55 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/LhtGml
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
+drop table WSC_fossils;
 CREATE TABLE "WSC_fossils" (
-    "Family" varchar   NOT NULL,
-    "Genus" varchar   NOT NULL,
-    "Species" varchar   NOT NULL,
-    "Rel_date" varchar   NOT NULL,
-    "locality" varchar   NOT NULL,
-    CONSTRAINT "pk_WSC_fossils" PRIMARY KEY (
-        "locality"
-     )
-);
+    "Family" varchar   ,
+	"Range" varchar   ,
+    "Genus" varchar   ,
+    "Species" varchar   ,
+    "locality" varchar   ,
+    "Rel_date" varchar   
 
+);
+drop table loc_data;
 CREATE TABLE "loc_data" (
-    "TypeLocality" varchar   NOT NULL,
-    "Amber" varchar   NOT NULL,
-    "Lat_dec" float NOT NULL,
-    "Long_dec" float NOT NULL,
-    CONSTRAINT "pk_loc_data" PRIMARY KEY (
-        "TypeLocality"
-     )
+    "TypeLocality" varchar   ,
+    "amber" varchar   ,
+    "lat_dec" varchar   ,
+    "long_dec" varchar   
 );
 
+drop table Dating_data;
 CREATE TABLE "Dating_data" (
-    "TypeLocality" varchar   NOT NULL,
-    "Era" varchar   NOT NULL,
-    "Period" varchar   NOT NULL,
-    "Epoch" varchar   NOT NULL,
-    "Age" varchar   NOT NULL,
-    "Abs_age" int   NOT NULL,
-    CONSTRAINT "pk_Dating_data" PRIMARY KEY (
-        "TypeLocality"
-     )
+    "TypeLocality" varchar   ,
+    "Era" varchar   ,
+    "Period" varchar   ,
+    "Epoch" varchar   ,
+    "Age" varchar  ,
+    "Abs_age" varchar   
 );
 
+drop table PBDB_Review;
 CREATE TABLE "PBDB_Review" (
-    "Family" varchar   NOT NULL,
-    "Genus" varchar   NOT NULL,
-    "Species" varchar   NOT NULL,
-    "Author" varchar   NOT NULL,
-    "Year" varchar   NOT NULL,
-    CONSTRAINT "pk_PBDB_Review" PRIMARY KEY (
-        "Genus","Species"
-     )
+    "Family" varchar  ,
+    "Genus" varchar   ,
+    "Species" varchar   ,
+    "Author" varchar  ,
+    "Year" varchar  
 );
 
--- ALTER TABLE "WSC_fossils" ADD CONSTRAINT "fk_WSC_fossils_Genus_Species" FOREIGN KEY("Genus", "Species")
--- REFERENCES "PBDB_Review" ("Genus", "Species");
+drop table Works_cited;
+CREATE TABLE "Works_cited" (
+	"author" varchar  ,
+	"pubyr" varchar  ,
+	"reftitle" varchar  ,
+	"pubtitle" varchar  ,
+	"pubvol" varchar  ,
+	"pubno" varchar  ,
+	"firstpage" varchar  ,
+	"lastpage" varchar  
+);
 
--- ALTER TABLE "WSC_fossils" ADD CONSTRAINT "fk_WSC_fossils_Rel_date" FOREIGN KEY("Rel_date")
--- REFERENCES "Dating_data" ("Period");
 
---  ALTER TABLE "loc_data" ADD CONSTRAINT "fk_loc_data_TypeLocality" FOREIGN KEY("TypeLocality")
--- 	REFERENCES "Dating_data" ("TypeLocality");
 
--- ALTER TABLE "loc_data" ADD CONSTRAINT "fk_loc_data_amber" FOREIGN KEY("amber")
--- REFERENCES "WSC_fossils" ("locality");
+
 
 
 

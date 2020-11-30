30a6a1804840baf19828860a92e9db81689bdf2a
Brittney Oleniacz <brittneyoleniacz@gmail.com>
2020-11-30 15:22:08 -0600

    ...

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request
 import json
 import psycopg2
 
@@ -11,29 +11,34 @@ conn = psycopg2.connect(
     password="postgres")
 
 
-@app.route("/Dating_data")
-def scrape():
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute("""   SELECT * FROM Dating_data """)
-    return json.dumps(cur.fetchall())
+# @app.route("/Dating_data")
+# def scrape():
+#     cur = conn.cursor(cursor_factory=RealDictCursor)
+#     cur.execute("""   SELECT * FROM Dating_data """)
+#     return json.dumps(cur.fetchall())
 
-@app.route("/PBDB_Review")
-def scrape():
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute("""   SELECT * FROM PBDB_Review """)
-    return json.dumps(cur.fetchall())
+# @app.route("/PBDB_Review")
+# def scrape():
+#     cur = conn.cursor(cursor_factory=RealDictCursor)
+#     cur.execute("""   SELECT * FROM PBDB_Review """)
+#     return json.dumps(cur.fetchall())
 
-@app.route("/works_cited")
-def scrape():
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute("""   SELECT * FROM works_cited """)
-    return json.dumps(cur.fetchall())
+# @app.route("/works_cited")
+# def scrape():
+#     cur = conn.cursor(cursor_factory=RealDictCursor)
+#     cur.execute("""   SELECT * FROM works_cited """)
+#     return json.dumps(cur.fetchall())
 
-@app.route("/WSC_fossils")
-def scrape():
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute("""   SELECT * FROM WSC_fossils """)
-    return json.dumps(cur.fetchall())
+# @app.route("/WSC_fossils")
+# def scrape():
+#     cur = conn.cursor(cursor_factory=RealDictCursor)
+#     cur.execute("""   SELECT * FROM WSC_fossils """)
+#     return json.dumps(cur.fetchall())
+
+@app.route("/")
+def home():
+    return render_template("present.html")
+
 
 @app.route("/loc_data")
 def scrape():
---
+++ static/js/map.js
@@ -0,0 +1,50 @@
+
+var myMap = L.map("map", {
+    center: [38.7223, 9.1393],
+    zoom: 3
+});
+
+//Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(myMap);
+console.log("hello")
+
+var coords = "/loc_data"
+
+d3.json(coords).then(function (data) {
+  console.log(data)
+})
+
+
+
+
+// Define variables for our tile layers
+var light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
+});
+
+var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "dark-v10",
+  accessToken: API_KEY
+});
+
+// Only one base layer can be shown at a time
+var baseMaps = {
+  Light: light,
+  Dark: dark
+};
+
+d3.json(coords, function(data){
+    console.log("hello")
+})
